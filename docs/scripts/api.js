aafb0db4d3bb9830760219ea0c4e4ff4ad669e24
Mattias Rylander <mattiasrylander@MattiasMacBook.local>
2026-05-06 16:11:43 +0200

    test kommunikation frontend-backend globalt

---
+++ docs/scripts/api.js
@@ -0,0 +1,29 @@
+// Hanterar kommunikationen med backend
+// Gör fetches
+
+const API_URL = "https://studyhelper-xlki.onrender.com";
+
+export function testFunction() {
+  return fetch(API_URL)
+    .then(res => res.text()); 
+}
+
+export function getBoards() {
+  return fetch(`${API_URL}/boards`)
+    .then(res => res.json());
+}
+
+export function getBoardById(id) {
+  return fetch(`${API_URL}/boards/${id}`)
+    .then(res => res.json());
+}
+
+export function createBoard(name) {
+  return fetch(`${API_URL}/boards`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name })
+  }).then(res => res.json());
+}
