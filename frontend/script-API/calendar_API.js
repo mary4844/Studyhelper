02f5ec5ee211fbeab1aee2de7267ebc34ed1f707
mary4844 <mattias.rylander.4844@student.uu.se>
2026-05-20 17:52:47 +0200

    ändra kalender_api fetch

--- frontend/script-API/calendar_API.js
+++ frontend/script-API/calendar_API.js
@@ -2,7 +2,7 @@
 console.log("calendar_API.js loaded");
 
 export async function getDeadlineTask(date) {
-    const response = await fetch(`http://localhost:3000/calendar/${date}`);
+    const response = await fetch(`/calendar/${date}`);
 
     if (!response.ok) {
         console.error("API error:", response.status);
